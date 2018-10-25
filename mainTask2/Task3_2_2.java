66e0e027cfc92028f9b9f09abca39d35b099ac48
Cinque666 <44259283+Cinque666@users.noreply.github.com>
2018-10-25 13:42:04 +0300

    Update Task3_2_2.java

--- mainTask2/Task3_2_2.java
+++ mainTask2/Task3_2_2.java
@@ -30,7 +30,6 @@ public class Task3_2_2 {
     
     public static boolean isNaturalNum(int num){
         if(num<0){
-            System.out.println("Число не является натуральным");
             return false;
         }
         else return true;
