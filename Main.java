7ca3a0e09fdd3712e94ea1c3ec744029af3de629
Cinque666 <44259283+Cinque666@users.noreply.github.com>
2018-11-07 21:35:07 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+package by.epam.javatraining.Zamoiski.maintask3;
+import java.util.Vector;
+/**
+ *
+ * @author Zamoiski Roman
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GeneralClass task3 = new GeneralClass();
+        //task3.printVector();
+        //task3.printlnVector();
+        //task3.maxElement();
+        //task3.minElement();
+        //task3.average();
+        //task3.geometricMean();
+        //task3.isAccending();
+        //task3.isDescending();
+        //task3.localMinimum();
+        //task3.localMaximum();
+        //task3.linearSearch(3);
+        //task3.binarySearch(3);
+        //task3.reverseVector();
+        //task3.bubbleSort();
+        //task3.selectionSort();
+        //task3.insertionSort();
+        //task3.printlnVector();
+        //task3.quickSort(0, 3);
+        AdditionalTask task3_2 = new AdditionalTask();
+        /*for(int i=0; i<task3_2.arr.length; i++){
+            for(int j=0; i<task3_2.arr[i].length; j++){
+                System.out.println("arr[" + i + "]" + "[" + j + "]=" + task3_2.arr[i][j]);
+            }
+        }*/
+        //System.out.println(task3_2.arr);
+    }
+}
