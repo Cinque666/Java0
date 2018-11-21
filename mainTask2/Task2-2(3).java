10b230ef9dc5a94b406ca8eb1a37068f6bb259a4
Cinque666 <44259283+Cinque666@users.noreply.github.com>
2018-11-21 07:51:30 +0300

    Update Task2-2(3).java

--- mainTask2/Task2-2(3).java
+++ mainTask2/Task2-2(3).java
@@ -1,18 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * copyrighted.
+ * 
+ * authors e-mail: roma.zamoiski@gmail.com
  */
 package task3.pkg3.pkg3;
 
 /**
  *
- * @author Roman
+ * @author Roman Zamoiski
  */
 public class Task3_3_3 {
 
     /**
-     * @param args the command line arguments
+     * Test
      */
     public static void main(String[] args) {
         // TODO code application logic here
