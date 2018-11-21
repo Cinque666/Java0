e44c500138d81474c9e755ce058eded784a7dc1d
Cinque666 <44259283+Cinque666@users.noreply.github.com>
2018-11-21 07:48:03 +0300

    Update Task2-1.java

--- mainTask2/Task2-1.java
+++ mainTask2/Task2-1.java
@@ -1,18 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * copyrighted.
+ * 
+ * authors e-mail: roma.zamoiski@gmail.com
  */
 package task3.pkg1;
 import java.util.Random;
 /**
  *
- * @author Roman
+ * @author Roman Zamoiski
  */
 public class Task31 {
 
     /**
-     * @param args the command line arguments
+     * Test method
      */
     public static void main(String[] args) {
         // TODO code application logic here
@@ -21,6 +21,10 @@ public class Task31 {
                 + " раз, решка: " + (1000-heads));
     }
     
+    /*
+    * 
+    * Calculate heads and tails
+    */
     public static int headsOrTails(int n){
         int a=0, heads=0;
         for(int i=0; i<n; i++){
@@ -33,5 +37,3 @@ public class Task31 {
     }
     
 }
-
-    
