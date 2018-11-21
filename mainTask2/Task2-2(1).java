6f2502410e0443306d1a22481086851a03cc0573
Cinque666 <44259283+Cinque666@users.noreply.github.com>
2018-11-21 07:50:43 +0300

    Update Task2-2(1).java

--- mainTask2/Task2-2(1).java
+++ mainTask2/Task2-2(1).java
@@ -1,18 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * copyrighted.
+ * 
+ * authors e-mail: roma.zamoiski@gmail.com
  */
 package task3.pkg2.pkg1;
 
 /**
  *
- * @author Roman
+ * @author Roman Zamoiski
  */
 public class Task3_2_1 {
 //boolean naturalNum;
     /**
-     * @param args the command line arguments
+     * Test method
      */
     public static void main(String[] args) {
         int x=6735;
@@ -21,13 +21,21 @@ public class Task3_2_1 {
         }
     }
     
+    /*
+    * 
+    * checks numbers for naturalness 
+    */
     public static boolean isNaturalNum(int num){
         if(num<=0){
             return false;
         }
         else return true;
     }
-        
+    
+    /*
+    * Finds greatest number 
+    * 
+    */
     public static int greatestNum(int num){
         int x=0, max=num%10;
         while(num > 0){
