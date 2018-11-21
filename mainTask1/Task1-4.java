3afcb58c9d6e59c34a4ef98124cdad3afd328fc7
Cinque666 <44259283+Cinque666@users.noreply.github.com>
2018-11-21 08:08:02 +0300

    Update Task1-4.java

--- mainTask1/Task1-4.java
+++ mainTask1/Task1-4.java
@@ -1,19 +1,16 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * copyrighted.
+ * 
+ * authors e-mail: roma.zamoiski@gmail.com
  */
 package task1.pkg4;
 import java.util.*;
 /**
  *
- * @author Roman
+ * @author Roman Zamoiski
  */
 public class Task14 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         //int[] arr = new int[4];
